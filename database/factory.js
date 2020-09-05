8f7cee89203aefe9386c472e58224ab286aa4960
Rafael Roberto <rafael.rp.silva@hotmail.com>
2020-09-05 17:50:06 -0300

    Factories

--- database/factory.js
+++ database/factory.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Factory = require('@adonisjs/lucid/src/Factory')
+
 /*
 |--------------------------------------------------------------------------
 | Factory
@@ -19,3 +21,27 @@
 //     username: faker.username()
 //   }
 // })
+
+Factory.blueprint('App/Models/User', faker => {
+    return {
+        name: faker.first(),
+        surname: faker.last(),
+        email: faker.email({ domain: 'fsocietybrasil.org' }),
+        password: 'secret'
+    }
+})
+
+Factory.blueprint('App/Models/Category', faker => {
+    return {
+        title: faker.country({full: true}),
+        description: faker.sentence()
+    }
+})
+
+Factory.blueprint('App/Models/Product', faker => {
+    return {
+        name: faker.animal(),
+        description: faker.sentence(),
+        price: faker.floating({ min: 0, max: 1000, fixed: 2})
+    }
+})
