a2fdd128be816a988a63f6f4bd2745dc568655a2
Rafael Roberto <rafael.rp.silva@hotmail.com>
2020-09-05 17:35:40 -0300

    Model de order e orderItems

---
+++ app/Models/Hooks/OrderHook.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const OrderHook = exports = module.exports = {}
+
+OrderHook.updatedValues = async (modelInstance) => {
+    model.$sideLoaded.subtotal = await model.items().getSum('subtotal')
+    model.$sideLoaded.qty_items = await model.items().getSum('quantity')
+    model.$sideLoaded.discount = await model.discounts().getSum('discount')
+    model.total = model.$sideLoaded.subtotal - model.$sideLoaded.discount
+}
+
+OrderHook.updateCollectionValues = async models => {
+
+    for (let model of models) {
+        model = await OrderHook.updatedValues(model)
+    }
+
+}
